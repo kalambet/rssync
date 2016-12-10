1f108010e782a32e9099c9d7ec0a6fa0c1d28182
Peter Kalambet <peter@technogeek.io>
2016-12-11 01:10:05 +0300

    Add macOS project skeleton

---
+++ RSSync/RSSync/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RSSync
+//
+//  Created by Peter Kalambet on 11/12/2016.
+//  Copyright © 2016 Peter Kalambet. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
